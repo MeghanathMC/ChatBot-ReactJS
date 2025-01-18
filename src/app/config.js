7ff74ce5ae311569cfdbcda8732a91bfde5f87c8
MeghanathMC <meghanath.mc24@gmail.com>
2025-01-18 18:57:07 +0530

    updated ui

--- src/app/config.js
+++ src/app/config.js
@@ -3,7 +3,7 @@ import { createChatBotMessage } from "react-chatbot-kit";
 const config ={
     botName: "card expert",
     initialMessages:[
-        createChatBotMessage("Hello I'm a credit card advisor,how can i help you")
+        createChatBotMessage("Hello I'm a charted accountant ,how can i help you")
     ]
 }
 
