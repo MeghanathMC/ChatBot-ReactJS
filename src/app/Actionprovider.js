0fa5f55456202d38ecefeee19451d5f4c9ed7f9b
MeghanathMC <meghanath.mc24@gmail.com>
2025-02-08 19:56:41 +0530

    bot-ui updated

--- src/app/Actionprovider.js
+++ src/app/Actionprovider.js
@@ -1,10 +1,10 @@
 import OpenAI from "openai";
 
 const openai = new OpenAI({
-    apiKey: "f0517ca61dca4c8889f7d8ccbb3a11e9",
-    baseURL: "https://api.aimlapi.com", // Ensure this URL is correct
-    dangerouslyAllowBrowser: true,
-});
+    apiKey: "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJGSjg2R2NGM2pUYk5MT2NvNE52WmtVQ0lVbWZZQ3FvcXRPUWVNZmJoTmxFIn0.eyJleHAiOjE3NzA1NDEzMzAsImlhdCI6MTczOTAwNTMzMCwianRpIjoiMTkyYWZhN2YtYmQxMi00MTMyLWE4MmYtMTczMDNjMjU1OGJkIiwiaXNzIjoiaHR0cDovL2dhdGV3YXkuZTJlbmV0d29ya3MuY29tL2F1dGgvcmVhbG1zL2FwaW1hbiIsImF1ZCI6ImFjY291bnQiLCJzdWIiOiIyOWJlYWVmOS0wOGIxLTQ4ZGYtOTFlOS1mOWQxYWZlYzI0ZTQiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJhcGltYW51aSIsInNlc3Npb25fc3RhdGUiOiJjNTkzMjRkNy1hYWU4LTQ3YzAtODFjMi1jZjFmYTZhZWE3ZDMiLCJhY3IiOiIxIiwiYWxsb3dlZC1vcmlnaW5zIjpbIiJdLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsib2ZmbGluZV9hY2Nlc3MiLCJ1bWFfYXV0aG9yaXphdGlvbiIsImFwaXVzZXIiLCJkZWZhdWx0LXJvbGVzLWFwaW1hbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoicHJvZmlsZSBlbWFpbCIsInNpZCI6ImM1OTMyNGQ3LWFhZTgtNDdjMC04MWMyLWNmMWZhNmFlYTdkMyIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwibmFtZSI6IkthcnRoaWsgTSIsInByaW1hcnlfZW1haWwiOiJra2FydGhpazc5MjNAZ21haWwuY29tIiwiaXNfcHJpbWFyeV9jb250YWN0Ijp0cnVlLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJra2FydGhpazc5MjNAZ21haWwuY29tIiwiZ2l2ZW5fbmFtZSI6IkthcnRoaWsiLCJmYW1pbHlfbmFtZSI6Ik0iLCJlbWFpbCI6ImtrYXJ0aGlrNzkyM0BnbWFpbC5jb20ifQ.eDZK8gP4q2Xk3ucB1cUhZET4QnA19r57vF9ZW1VmMkBX-kwgRmHUBgY3vUbnY6dWRWRP8zW9cxzDy437ZxtLq3oPG0wswORahWUzIzZ2B-qufDt5kdR2I-MPTmm9ZgakzPhEwPORuJQjazTQN3AdWNslSCTr-DTIDRX261LisuI", 
+    baseURL: "https://infer.e2enetworks.net/project/p-4816/genai/deepseek_v3/v1",
+    dangerouslyAllowBrowser:true
+  });
 
 class Actionprovider {
     createChatBotMessage;
@@ -31,7 +31,7 @@ class Actionprovider {
     callGenAI = async (prompt) => {
         try {
             const ChatCompletion = await openai.chat.completions.create({
-                model: "gpt-3.5-turbo",
+                model: "deepseek_v3",
                 messages: [
                     { role: "system", content: "You are a medical expert who is helping the user to get the best treatment for their condition" },
                     { role: "user", content: prompt },
