aca83c8fa6977b528c4c69cdee8c1c85541fca57
MeghanathMC <meghanath.mc24@gmail.com>
2025-02-08 20:10:23 +0530

    final-commit

--- src/app/Actionprovider.js
+++ src/app/Actionprovider.js
@@ -36,8 +36,8 @@ class Actionprovider {
                     { role: "system", content: "You are a medical expert who is helping the user to get the best treatment for their condition" },
                     { role: "user", content: prompt },
                 ],
-                temperature: 0.7,
-                max_tokens: 50,
+                temperature: 0.5,
+                max_tokens: 300,
             });
             return ChatCompletion.choices[0].message.content;
         } catch (error) {
@@ -48,30 +48,25 @@ class Actionprovider {
 
     timer = (ms) => new Promise((res) => setTimeout(res, ms));
 
-    generateResponseMessage = async (userMessage) => {
-        try {
-            const responseGPT = await this.callGenAI(userMessage);
-            let message;
-            const numberNoLines = responseGPT.split("\n").length;
+generateResponseMessage = async (userMessage) => {
+    try {
+        const responseGPT = await this.callGenAI(userMessage);
 
-            for (let i = 0; i < numberNoLines; i++) {
-                const msg = responseGPT.split("\n")[i];
-                if (msg.length) {
-                    message = this.createChatBotMessage(msg);
-                    this.updateChatBotMessage(message);
-                }
-                await this.timer(1000); // Wait for 1 second before sending the next message
-            }
-        } catch (error) {
-            console.error("Error in generating response message:", error);
-        }
-    };
+        // Create a single message instead of looping
+        const message = this.createChatBotMessage(responseGPT);
+        this.updateChatBotMessage(message);
+        
+    } catch (error) {
+        console.error("Error in generating response message:", error);
+    }
+};
 
-    respond = (message) => {
-        this.generateResponseMessage(message);
-    };
+respond = (message) => {
+    this.generateResponseMessage(message);
+};
 
-    updateChatBotMessage = (message) => {
+
+     updateChatBotMessage = (message) => {
         this.setState((prevState) => ({
             ...prevState,
             messages: [...prevState.messages, message],
